55038eed98fc706607f33fb7650ef6498ce671fb
Rocktim <rajkumar.rocktim@gmail.com>
2019-09-07 10:31:09 +0530

    main.js : clear item event send

--- main.js
+++ main.js
@@ -83,7 +83,10 @@ const mainMenuTemplate = [
                 }
             },
             {
-                label: 'Clear Item'
+                label: 'Clear Item',
+                click() {
+                    mainWindow.webContents.send('item:clear');
+                }
             },
             {
                 label: 'Quit',
